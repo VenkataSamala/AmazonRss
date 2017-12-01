b5b45acd4392dc4934a60efd1baffd81ff3d28ff
Venkata Samala <venkata_samala@outlook.com>
2017-11-30 23:16:25 -0500

    Add project files.

---
+++ AmazonRss/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace AmazonRss.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ AmazonRss/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AmazonRss.Global" Language="C#" %>
